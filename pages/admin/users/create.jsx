9adf0f894a52828d8351f8a6943b46d4a4a7098c
minhduy77 <104126672+minhduy77@users.noreply.github.com>
2024-06-03 23:53:07 +0700

    Create User Update

--- pages/admin/users/create.jsx
+++ pages/admin/users/create.jsx
@@ -1,44 +1,68 @@
 import { Box } from "@mui/material";
 import * as yup from "yup";
 import { H3 } from "components/Typography";
-import { ProductForm } from "pages-sections/admin";
-import VendorDashboardLayout from "components/layouts/vendor-dashboard"; // =============================================================================
+import VendorDashboardLayout from "components/layouts/vendor-dashboard";
+import AccountForm from "../../../src/pages-sections/admin/accounts/AccountForm";
+import axios from "axios"; // =============================================================================
 
-CreateProduct.getLayout = function getLayout(page) {
+CreateAccount.getLayout = function getLayout(page) {
   return <VendorDashboardLayout>{page}</VendorDashboardLayout>;
 }; // =============================================================================
 
-export default function CreateProduct() {
+export default function CreateAccount() {
   const INITIAL_VALUES = {
+    userName: "",
     name: "",
-    tags: "",
-    stock: "",
-    price: "",
-    category: "",
-    sale_price: "",
-    description: "",
+    address: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+    roleId: "",
+    counterId: "",
   };
   const validationSchema = yup.object().shape({
     name: yup.string().required("required"),
-    category: yup.string().required("required"),
-    description: yup.string().required("required"),
-    stock: yup.number().required("required"),
-    price: yup.number().required("required"),
-    sale_price: yup.number().required("required"),
-    tags: yup.object().required("required"),
+    userName: yup.string().required("required"),
+    address: yup.string().required("required"),
+    email: yup.string().required("required"),
+    password: yup.string().required("required"),
+    phoneNumber: yup.string().required("required"),
+    roleId: yup.string().required("required"),
+    counterId: yup.string().required("required"),
   });
 
-  const handleFormSubmit = () => {};
-
+  const handleFormSubmit = async (values) => {
+    const accountNew = {
+      name: values.name,
+      userName: values.userName,
+      address: values.address,
+      email: values.email,
+      password: values.password,
+      phoneNumber: values.phoneNumber,
+      roleId: values.roleId,
+      counterId: values.counterId,
+    }
+    try {
+      const token = localStorage.getItem('token')
+      const response = await axios.post("https://four-gems-api-c21adc436e90.herokuapp.com/user/signup", accountNew,
+          {
+            headers: {
+              Authorization: 'Bearer ' + token
+            },
+          })
+    } catch (e) {
+      console.log(e)
+    }
+  };
   return (
-    <Box py={4}>
-      <H3 mb={2}>Add New User</H3>
+      <Box py={4}>
+        <H3 mb={2}>Add New User</H3>
 
-      <ProductForm
-        initialValues={INITIAL_VALUES}
-        validationSchema={validationSchema}
-        handleFormSubmit={handleFormSubmit}
-      />
-    </Box>
+        <AccountForm
+            initialValues={INITIAL_VALUES}
+            validationSchema={validationSchema}
+            handleFormSubmit={handleFormSubmit}
+        />
+      </Box>
   );
 }
